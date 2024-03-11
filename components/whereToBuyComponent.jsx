ed0bb20422513654908cad0fc656483ce36fb56a
Zbynek Svoboda <wentura17@seznam.cz>
2024-03-11 16:39:40 +0100

    loga kde se koupit

--- components/whereToBuyComponent.jsx
+++ components/whereToBuyComponent.jsx
@@ -2,39 +2,51 @@ import React from "react";
 const logos = [
   {
     title: "Nerashop",
-    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1709372244/diavita/nerashop_ts-1_rjutwo.webp",
+    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1710171399/diavita/nerashop_dpdtnf.png",
     icon: "icon",
     link: "https://www.nerashop.cz/znacka/diavita/",
   },
   {
-    title: "Nerashop",
-    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1709372244/diavita/nerashop_ts-1_rjutwo.webp",
+    title: "Alphega",
+    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1710170616/diavita/alphega_jtuvun.png",
     icon: "icon",
-    link: "https://www.nerashop.cz",
+    link: "https://www.alphega.cz/vysledky-hledani?search=valinka&s=0",
   },
   {
-    title: "Nerashop",
-    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1709372244/diavita/nerashop_ts-1_rjutwo.webp",
+    title: "Benu",
+    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1710170616/diavita/benu_tz4faw.png",
     icon: "icon",
-    link: "https://www.nerashop.cz",
+    link: "https://www.benu.cz/vyhledavani?q=valinka",
   },
   {
-    title: "Nerashop",
-    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1709372244/diavita/nerashop_ts-1_rjutwo.webp",
+    title: "Dr. Max",
+    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1710170616/diavita/dr_max_dan2so.png",
     icon: "icon",
-    link: "https://www.nerashop.cz",
+    link: "https://www.drmax.cz/vyhledavani?q=valinka",
   },
   {
-    title: "Nerashop",
-    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1709372244/diavita/nerashop_ts-1_rjutwo.webp",
+    title: "Giga lékárna",
+    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1710170688/diavita/giga_lekarna_xgi1d8.png",
     icon: "icon",
-    link: "https://www.nerashop.cz",
+    link: "https://www.gigalekarna.cz/vyhledavani/?phrase=valinka",
   },
   {
-    title: "Nerashop",
-    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1709372244/diavita/nerashop_ts-1_rjutwo.webp",
+    title: "Lékárna",
+    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1710170616/diavita/lekarna_cz_lsabtw.png",
     icon: "icon",
-    link: "https://www.nerashop.cz",
+    link: "https://www.lekarna.cz/valinka/?fromSearch=true",
+  },
+  {
+    title: "Moje lékárna",
+    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1710170616/diavita/moje_lekarna_c3xamn.png",
+    icon: "icon",
+    link: "https://www.mojelekarna.cz/vyhledavani?s=valinka",
+  },
+  {
+    title: "Pilulka",
+    logo: "https://res.cloudinary.com/dam7wdzvx/image/upload/v1710170616/diavita/pilulka_rqn7ro.png",
+    icon: "icon",
+    link: "https://www.pilulka.cz/hledej?q=valinka",
   },
 ];
 export default function WhereToBuyComponent() {
@@ -52,15 +64,15 @@ export default function WhereToBuyComponent() {
           </p> */}
         </div>
 
-        <div className="grid grid-cols-2 gap-4 rounded-lg md:grid-cols-3 lg:gap-6">
+        <div className="grid grid-cols-2 gap-4 rounded-lg md:grid-cols-4 lg:gap-6">
           {logos.map((logo) => {
             return (
               <a
                 href={logo.link}
-                className="flex h-16 items-center justify-center rounded-lg bg-white p-4 text-gray-400 sm:h-32 border border-gray-100"
+                className="flex h-16 items-center justify-center rounded-lg bg-white p-4 text-gray-400 sm:h-32 hover:scale-110 transition duration-300"
                 key={logo.title}
               >
-                <img src={logo.logo} alt={logo.text} />
+                <img src={logo.logo} alt={logo.text} className="max-h-12" />
               </a>
             );
           })}
