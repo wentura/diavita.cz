c8543d4c255b840870e44516a6df7e1179c20a36
Zbynek Svoboda <wentura17@seznam.cz>
2024-05-13 11:57:09 +0200

    hero img neprekryva menu nav

--- components/hero.jsx
+++ components/hero.jsx
@@ -15,7 +15,7 @@ export default function HeroComponent() {
           </div>
 
           <div className="mb-12 hidden sm:flex w-2/3 md:mb-16 lg:w-1/2 mx-auto">
-            <div className="relative left-2 top-12 z-10 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-8 md:top-16 lg:ml-0">
+            <div className="relative left-2 top-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-8 md:top-16 lg:ml-0">
               <img
                 src="https://res.cloudinary.com/dam7wdzvx/image/upload/v1709371071/diavita/1.webp"
                 // src="https://res.cloudinary.com/dam7wdzvx/image/upload/v1709371071/diavita/ybpn681gqiledc4y0uhv-md_vzeoh5.webp"
