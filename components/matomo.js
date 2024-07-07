0167f79f85f9682d89a40a58f91c8a51440bbd33
Zbynek Svoboda <wentura17@seznam.cz>
2024-07-07 10:56:48 +0200

    maotmo https a opacity-0

--- components/matomo.js
+++ components/matomo.js
@@ -1,11 +1,12 @@
+"use client";
 export default function Matomo() {
   return (
     <div>
       <img
         referrerPolicy="no-referrer-when-downgrade"
-        src="http://matomo.zbyneksvoboda.cz/matomo.php?idsite=13&amp;rec=1"
-        className="border-0"
-        alt=""
+        src="https://matomo.zbyneksvoboda.cz/matomo.php?idsite=13"
+        className="border-0 opacity-0"
+        alt="matomo img counter"
       />
     </div>
   );
